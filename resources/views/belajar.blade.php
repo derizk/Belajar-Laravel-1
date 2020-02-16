7aa6e2564ac1a1e7571402b88ed95f3fbf23d610
derizk <dewirizk32@gmail.com>
2020-02-16 12:53:32 +0700

    test

---
+++ resources/views/belajar.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Belajar Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .content {
+                text-align: center;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .title {
+                font-size: 50px;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <div class="content">
+            
+            <div class="title">
+                <h1>Belajar Laravel #1</h1>
+            </div>
+
+            <p>Lorem Ipsum bla bla bla</p>
+
+            <div class="links">
+                <a href="">Beginer</a>
+                <a href="">Middle</a>
+                <a href="">Advance</a>
+            </div>   
+        </div>
+
+    </body>
+</html>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <p> halooo </p>
+    
+@endsection
